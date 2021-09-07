ce94ef2ed042962a8e7fc0aed248a24307928be4
Kirill <tor505274@gmail.com>
2021-09-07 10:38:56 +0300

    Добавьте файлы проекта.

---
+++ cg_task1/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿
+namespace cg_task1
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.BackColor = System.Drawing.Color.White;
+            this.pictureBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(582, 453);
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(582, 453);
+            this.Controls.Add(this.pictureBox);
+            this.Name = "Form1";
+            this.Text = "Form";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox;
+    }
+}
+
---
+++ cg_task1/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace cg_task1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Draw();
+        }
+
+        private void Draw()
+        {
+            Bitmap bmp = new Bitmap(pictureBox.Width, pictureBox.Height);
+            DrawGradientImage(bmp, Color.Aqua, Color.Yellow);
+            Graphics g = Graphics.FromImage(bmp);
+            Pen pen = new Pen(Color.Red);
+            g.FillRectangle(Brushes.Red, 10, 50, 1, 1);
+            g.DrawLine(pen, 10, 10, 100, 100);
+            g.DrawRectangle(pen, 300, 50, 100, 300);
+            g.DrawEllipse(pen, 150, 100, 100, 60);
+            pictureBox.Image = bmp;
+        }
+
+        private static void DrawGradientImage(Bitmap bmp,Color startColor, Color endColor)
+        {
+            for (int x = 0; x < bmp.Width; x++)
+            {
+                int r = (int)Math.Floor((double)(startColor.R + (endColor.R - startColor.R) * x / bmp.Width));
+                int g = (int)Math.Floor((double)(startColor.G + (endColor.G - startColor.G) * x / bmp.Width));
+                int b = (int)Math.Floor((double)(startColor.B + (endColor.B - startColor.B) * x / bmp.Width));
+                Color current = Color.FromArgb(r, g, b);
+
+                for (int y = 0; y < bmp.Height; y++)
+                {
+                    bmp.SetPixel(x, y, current);
+                }
+            }
+        }
+    }
+}
